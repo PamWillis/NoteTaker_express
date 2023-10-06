16310116e38dd47ba307afc97901fff72f440da9
Pam <pwillisop@yahoo.com>
2023-10-06 16:08:02 -0400

    added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+//initialize the app and create port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/api', htmlRoutes);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
