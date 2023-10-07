6c8b6d441d0d97b7ffa3625799a3a954bb0df9ec
Pam <pwillisop@yahoo.com>
2023-10-07 16:19:57 -0400

    change app.use

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./public/route/apiRoutes.js');
 const htmlRoutes = require('./public/route/htmlRoutes.js');
+const path = require('path')
 
 //initialize the app and create port
 const app = express();
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
@@ -17,4 +18,3 @@ app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-//delete later
