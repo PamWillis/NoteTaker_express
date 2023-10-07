130f0e23d6ebac12db9921578701bd81d4496cd8
Pam <pwillisop@yahoo.com>
2023-10-07 15:27:02 -0400

    changed html route

--- public/route/htmlRoutes.js
+++ public/route/htmlRoutes.js
@@ -2,8 +2,8 @@ const path = require('path');
 const router = require('express').Router();
 
 // "/notes" responds with notes.html file
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // All other routes respond with the index.html file
