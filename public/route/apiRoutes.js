280ec8facaea571d5d222327fb9916a87f5ec75e
Pam <pwillisop@yahoo.com>
2023-10-08 19:46:21 -0400

    fixed routers

--- public/route/apiRoutes.js
+++ public/route/apiRoutes.js
@@ -1,47 +1,48 @@
 const path = require('path');
 const fs = require('fs');
+const app = require('express').Router();
 
 //this package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
 var uniqid = require('uniqid');
 
-module.exports = (app) => {
-
-  app.get('/api/notes', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-  });
 
 
-
-  //POST/api/notes - should receive a note to save on the request body, at it to the db.json and return a note to client
-  app.post('/api/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    //create note
-    let userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      //create id
-      id: uniqid(),
-    };
-
-    //push created note to be added to the db.json file
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
-
-  });
+app.get('/api/notes', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
 
 
-  //DELETE "api/notes/:id" should receive query containing id for note to delete.
-  app.delete('/api/notes/:id', (req, res) => {
-    //reading notes from db.json
-    let db = JSON.parse(fs.readFileSync('db/db.json'))
-    // removing note with id
-    let deleteNotes = db.filter(item => item.id !== req.params.id);
-    // Rewriting note to db.json
-    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-    res.json(deleteNotes);
-  });
 
-};
+//POST/api/notes - should receive a note to save on the request body, at it to the db.json and return a note to client
+app.post('/api/notes', (req, res) => {
+  let db = fs.readFileSync('db/db.json');
+  db = JSON.parse(db);
+  res.json(db);
+  //create note
+  let userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    //create id
+    id: uniqid(),
+  };
+
+  //push created note to be added to the db.json file
+  db.push(userNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(db));
+  res.json(db);
+
+});
+
+
+//DELETE "api/notes/:id" should receive query containing id for note to delete.
+app.delete('/api/notes/:id', (req, res) => {
+  //reading notes from db.json
+  let db = JSON.parse(fs.readFileSync('db/db.json'))
+  // removing note with id
+  let deleteNotes = db.filter(item => item.id !== req.params.id);
+  // Rewriting note to db.json
+  fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
+const apiRoutes = require('./public/route/apiRoutes');
+const htmlRoutes = require('./public/route/htmlRoutes');
 
-//app use and create port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// asks express to create a route for every file in the 'public' folder and give it a '/' route
-app.use(express.static('public'));
-// sets up express app to handel data parser, middle wear created req.body
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// routes to route files
-require('./route/apiRoutes')(app);
-require('./route/htmlRoutes')(app);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //app listener to start the server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
