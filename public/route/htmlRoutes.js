a1e063d112cf0b1ac158551b2808df851735264e
Pam <pwillisop@yahoo.com>
2023-10-08 11:26:16 -0400

    changed display to visible on icons

--- public/route/htmlRoutes.js
+++ public/route/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require('path');
 const router = require('express').Router();
 
 // "/notes" responds with notes.html file
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,  '../public/notes.html'));
-});
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../notes.html'));
+  });
 
 // All other routes respond with the index.html file
 router.get('*', (req, res) => {
